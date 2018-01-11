fcc8c7abb76b60148290e456981b88f4e15cef8d
Amanda Wehrman <arwehrman@gmail.com>
2018-01-11 19:20:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function printBadges(names) {
   for (let i = 0; i < names.length; i++) {
     name = names[index]
-console.log('Welcome ${name}! You are employee #{index + 1}.')
+console.log('Welcome ${name}! You are employee #${index + 1}.')
   }
   return names;
 }
