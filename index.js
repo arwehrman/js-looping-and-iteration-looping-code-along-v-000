d84b0cc34a5a55690af182cea657ccf16b376c26
Amanda Wehrman <arwehrman@gmail.com>
2018-01-11 19:22:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function printBadges(employee_array){
-    for (let i = 0; i < employee_array.length; i++) {
-        employee = employee_array[i]
-        console.log(`Welcome ${employee}! You are employee #${i + 1}.`)
-    }
-    return employee_array
+  for (let i = 0; i < employee_array.length; i++) {
+      employee = employee_array[i]
+      console.log(`Welcome ${employee}! You are employee #${i + 1}.`)
+  }
+  return employee_array
 }
